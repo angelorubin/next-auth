d409b6b96b3f6dcb4d8cef10f9f22dee1acd2cc4
JonsMota <jonsmota@gmail.com>
2023-03-05 14:30:27 -0300

    Criado os componentes input e button, e o Link

--- pages/cadastro.js
+++ pages/cadastro.js
@@ -1,3 +1,9 @@
+import Link from 'next/link'
+
+import Button from '../src/components/button/button'
+import Input from '../src/components/input/input'
+
+
 import styles from '../styles/Login.module.css'
 
 import LoginCard from '../src/components/loginCard/loginCard'
@@ -5,7 +11,15 @@ import LoginCard from '../src/components/loginCard/loginCard'
 export default function CadastroPage() {
     return (
       <div className={styles.background}>
-        <LoginCard />
+        <LoginCard title="Crie sua conta">
+          <form className={styles.form}>
+            <Input type="text" placeholder="Seu nome" required />
+            <Input type="email" placeholder="Seu e-mail" required />
+            <Input type="password" placeholder="Sua senha" required />
+            <Button>Cadastrar</Button>
+            <Link href="/cadastro">Já possui conta?</Link>
+          </form>
+        </LoginCard>
       </div>
     )    
   }  
--- pages/login.js
+++ pages/login.js
@@ -1,3 +1,8 @@
+import Link from 'next/link'
+
+import Button from '../src/components/button/button'
+import Input from '../src/components/input/input'
+
 import styles from '../styles/Login.module.css'
 
 import LoginCard from '../src/components/loginCard/loginCard'
@@ -5,7 +10,14 @@ import LoginCard from '../src/components/loginCard/loginCard'
 export default function LoginPage() {
   return (
     <div className={styles.background}>
-      <LoginCard />
+      <LoginCard title="Entre em sua conta">
+        <form className={styles.form}>
+          <Input type="email" placeholder="Seu e-mail" required />
+          <Input type="password" placeholder="Sua senha" required />
+          <Button>Entrar</Button>
+	      <Link href="/cadastro">Ainda não possui conta?</Link>
+        </form>
+      </LoginCard>
     </div>
   )    
 }  
