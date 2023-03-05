6fa3cee64b169db35078d64b9d72ee0a7478202e
JonsMota <jonsmota@gmail.com>
2023-03-05 19:11:51 -0300

    Feito validação do token para liberar o acesso a página

--- pages/login.js
+++ pages/login.js
@@ -1,21 +1,55 @@
+import { useState } from 'react'
 import Link from 'next/link'
+import { setCookie } from 'cookies-next'
+import { useRouter } from 'next/router'
 
 import Button from '../src/components/button/button'
 import Input from '../src/components/input/input'
+
 
 import styles from '../styles/Login.module.css'
 
 import LoginCard from '../src/components/loginCard/loginCard'
 
 export default function LoginPage() {
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  })  
+
+  const [error, setError] = useState('')
+  const router = useRouter()
+
+  const handleFormEdit = (event, name) => {
+    setFormData({...formData, [name]: event.target.value})
+  }
+
+  const handleForm = async (event) => {
+    try {
+      event.preventDefault()
+      const response = await fetch(`/api/user/login`, {
+        method: 'POST',
+        body: JSON.stringify(formData)
+      })
+      const json = await response.json()
+      if (response.status !== 200) throw new Error(json)
+
+      setCookie('authorization', json)
+      router.push('/')  
+    } catch (err)   {
+      setError(err.message) 
+    }
+  }
+
   return (
     <div className={styles.background}>
       <LoginCard title="Entre em sua conta">
-        <form className={styles.form}>
-          <Input type="email" placeholder="Seu e-mail" required />
-          <Input type="password" placeholder="Sua senha" required />
-          <Button>Entrar</Button>
-	      <Link href="/cadastro">Ainda não possui conta?</Link>
+        <form onSubmit={handleForm} className={styles.form}>
+          <Input type="email" placeholder="Seu e-mail" required value={formData.email}  onChange={(e) => {handleFormEdit(e, 'email')}} />
+          <Input type="password" placeholder="Sua senha" required value={formData.password} onChange={(e) => {handleFormEdit(e, 'password')}} />
+          <Button>Cadastrar</Button>
+          {error && <p className={styles.error}>{error}</p>}
+          <Link href="/cadastro">Ainda não possui conta?</Link>
         </form>
       </LoginCard>
     </div>
