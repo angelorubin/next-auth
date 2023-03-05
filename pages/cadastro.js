acb8d280601705bd8871666582191c80aca49bc6
JonsMota <jonsmota@gmail.com>
2023-03-05 15:36:44 -0300

    Criado formulário para pegar os dados digitados

--- pages/cadastro.js
+++ pages/cadastro.js
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import Link from 'next/link'
 
 import Button from '../src/components/button/button'
@@ -9,17 +10,32 @@ import styles from '../styles/Login.module.css'
 import LoginCard from '../src/components/loginCard/loginCard'
 
 export default function CadastroPage() {
-    return (
-      <div className={styles.background}>
-        <LoginCard title="Crie sua conta">
-          <form className={styles.form}>
-            <Input type="text" placeholder="Seu nome" required />
-            <Input type="email" placeholder="Seu e-mail" required />
-            <Input type="password" placeholder="Sua senha" required />
-            <Button>Cadastrar</Button>
-            <Link href="/login">Já possui conta?</Link>
-          </form>
-        </LoginCard>
-      </div>
-    )    
-  }  
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+  })  
+
+  const handleFormEdit = (event, name) => {
+    setFormData({...formData, [name]: event.target.value})
+  }
+
+  const handleForm = (event) => {
+    event.preventDefault()
+    console.log(formData)
+  }
+
+  return (
+    <div className={styles.background}>
+      <LoginCard title="Crie sua conta">
+        <form onSubmit={handleForm} className={styles.form}>
+          <Input type="text" placeholder="Seu nome" required value={formData.name} onChange={(e) => {handleFormEdit(e, 'name')}} />
+          <Input type="email" placeholder="Seu e-mail" required value={formData.email}  onChange={(e) => {handleFormEdit(e, 'email')}} />
+          <Input type="password" placeholder="Sua senha" required value={formData.password} onChange={(e) => {handleFormEdit(e, 'password')}} />
+          <Button>Cadastrar</Button>
+          <Link href="/login">Já possui conta?</Link>
+        </form>
+      </LoginCard>
+    </div>
+  )    
+}  
