81e28c03ec38e95b20bd6ee5cfff30b8b98aabc3
JonsMota <jonsmota@gmail.com>
2023-04-15 12:18:24 -0300

    2

--- pages/api/user/login.js
+++ pages/api/user/login.js
@@ -1,8 +1,8 @@
-import { login } from '../../../services/user'
+import { loginUser } from '../../../services/user'
 
 export default function handler(req, res) {
   try {
-    const user = login(req.body)
+    const user = loginUser(req.body)
     res.status(200).json(user)
   } catch (err) {
     res.status(400).json(err.message)
