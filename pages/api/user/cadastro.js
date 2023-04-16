769cb9ff3f8358d9bb57b3bef75af9405780d83c
JonsMota <jonsmota@gmail.com>
2023-04-16 14:54:53 -0300

    Atualizado

--- pages/api/user/cadastro.js
+++ pages/api/user/cadastro.js
@@ -2,7 +2,7 @@ import { cadastroUser } from '@/services/user'
 
 export default function handler(req, res) {
   try {
-    const newUser = cadastroUser(req.body)
+    const newUser = cadastroUser(JSON.parse(req.body))
     res.status(201).json(newUser)
   } catch (err) {
     res.status(400).json(err.message)
--- services/user.js
+++ services/user.js
@@ -3,18 +3,18 @@ import User from '../models/user'
 
 export async function cadastroUser(body) {
   await databaseConnection()
-  const user = await User.find(({ email }) => email === body.email)
-  if (user) throw new Error('Usuário já cadastrado')
-
-  User.push(body)
+  const user = await User.find({ email: body.email })
+  if (user.length !== 0) throw new Error('Usuário já cadastrado')
+  const newUser = new User(body)
+  User.create(newUser)
   return body
 }
 
 export async function loginUser(body) {
   await databaseConnection()
-  const user = User.find(({ email }) => email === body.email)
-  if (!user) throw new Error('Usuário não encontrado')
-  if (user.password !== body.password) throw new Error('Senha incorreta')
+  const user = await User.find({ email: body.email })
+  if (user.length === 0) throw new Error('Usuário não encontrado')
+  if (user[0].password !== body.password) throw new Error('Senha incorreta')
 
   return user
 }
