07a17306800f8ce61223658d0d8494195c68aba1
Angelo Rubin <angelorubin@gmail.com>
2023-05-20 15:01:31 -0300

    Injetando fonte Roboto

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,14 @@
 // These styles apply to every route in the application
 import './globals.css'
+import { Roboto } from 'next/font/google'
+
+const roboto = Roboto({
+  weight: ['100', '300', '400', '500', '700', '900'],
+  style: ['normal'],
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-roboto'
+})
 
 export const metadata = {
   title: 'Next Auth',
@@ -8,7 +17,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" className={`${roboto.variable}`}>
       <body>{children}</body>
     </html>
   )
