3a4036309587ae4bf7078a7fe3fb086e73e16c54
angelorubin <angelorubin@gmail.com>
2023-05-22 08:41:12 -0300

    Mantendo estrutura da API na versão pages/api do Next

--- app/api/register/service.js
+++
@@ -1,8 +0,0 @@
-import databaseConnection from '../../utils/database'
-import User from '../../models/user'
-
-export async function userRetrieve(email) {
-  await databaseConnection()
-  const user = await User.find({ email })
-  return user
-}
--- app/auth/page.jsx
+++ app/auth/page.jsx
@@ -3,8 +3,8 @@ import Link from 'next/link'
 import axios from 'axios'
 import { useState } from 'react'
 import { useRouter } from 'next/navigation'
-import Input from '../../components/input'
-import Button from '../../components/button'
+import Input from '../components/input'
+import Button from '../components/button'
 import { setCookie } from 'nookies'
 
 export default function Auth() {
