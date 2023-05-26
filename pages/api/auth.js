512945b8f6d3a206edafa5042a67ff7566dbd7f2
Angelo Rubin <angelorubin@gmail.com>
2023-05-26 16:33:27 -0300

    Removendo arquivos não utilizados

--- pages/api/auth.js
+++
@@ -1,9 +0,0 @@
-import { auth } from '../../app/services/auth'
-
-export default async function handler(req, res) {
-  if (req.method === 'POST') {
-    const { email, password } = req.body
-    const user = await auth(email, password)
-    return res.json(user)
-  }
-}
