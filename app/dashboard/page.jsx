1c4aee272ab3621b45ca5100bfac388d89e091f7
angelorubin <angelorubin@gmail.com>
2023-05-28 16:51:24 -0300

    Implementando middleware de autenticação

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,3 +1,10 @@
-export default function Dashboard() {
-  return <h1>Welcome to Dashboard!!!</h1>
+import { parseCookies } from 'nookies'
+
+export default function Dashboard(props) {
+  const { token } = props
+  return (
+    <>
+      <h1>Welcome to Dashboard!!!</h1>
+    </>
+  )
 }
