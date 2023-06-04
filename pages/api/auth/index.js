4a994825757bbd29a6bc5decd17e18f2a1089983
angelorubin <angelorubin@gmail.com>
2023-06-04 16:26:36 -0300

    Ajustando resposta do endpoint register em caso de dados inválidos de autenticação

--- pages/api/auth/index.js
+++ pages/api/auth/index.js
@@ -5,11 +5,9 @@ export default async function handler(req, res) {
 
   if (req.method === 'POST') {
     try {
-      const response = await userAuthentication(email, password)
-      if (response) {
-        res.status(200).json({ token: response })
-      } else {
-        res.status(401).json({ message: 'Acesso não autorizado, credenciais inválidas.' })
+      const token = await userAuthentication(email, password)
+      if (token) {
+        res.status(200).json({ token })
       }
     } catch (error) {
       res.status(401).json({ message: 'Acesso não autorizado, credenciais inválidas.' })
