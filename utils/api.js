901efcbd10943d3d91d2fe60e3ce833faeb62bb4
Angelo Rubin <angelorubin@gmail.com>
2023-06-05 17:37:30 -0300

    Ajustando URL da API

--- utils/api.js
+++ utils/api.js
@@ -4,7 +4,7 @@ import { parseCookies } from 'nookies'
 const token = parseCookies('token')
 
 export const api = axios.create({
-  baseURL: 'http://localhost:3000/api'
+  baseURL: 'https://vercel.com/angelorubin/next-auth/api'
 })
 
 api.defaults.headers.common['Authorization'] = `Bearer ${token}`
