10099fc79ee673ccbd4a1e16e81fb506ac0aa624
Angelo Rubin <angelorubin@gmail.com>
2023-06-05 16:13:47 -0300

    Removendo else não utilizado

--- pages/api/auth/index.js
+++ pages/api/auth/index.js
@@ -6,10 +6,9 @@ export default async function handler(req, res) {
   if (req.method === 'POST') {
     try {
       const response = await userAuthentication(email, password)
+
       if (response) {
         res.status(200).json({ token: response })
-      } else {
-        res.status(401).json({ message: 'Acesso não autorizado, credenciais inválidas.' })
       }
     } catch (error) {
       res.status(401).json({ message: 'Acesso não autorizado, credenciais inválidas.' })
