f7e43a6a9da10f75ef706ce40196212a04534708
Angelo Rubin <angelorubin@gmail.com>
2023-06-07 15:29:32 -0300

    Removendo importação não utilizada

--- pages/dashboard/index.jsx
+++ pages/dashboard/index.jsx
@@ -1,12 +1,38 @@
 import { useRouter } from 'next/router'
 import { parseCookies, destroyCookie } from 'nookies'
 import { AiOutlineLogout } from 'react-icons/ai'
-import fetch from 'isomorphic-fetch'
+import { api } from '../../utils/api'
 
 export async function getServerSideProps(context) {
   const cookies = parseCookies(context)
   const token = cookies.token // Obtém o token dos cookies
 
+  const res = await fetch('http:localhost:3000/api/validate-token', {
+    method: 'POST',
+    headers: {
+      Authorization: `Bearer ${token}`
+    }
+  })
+
+  const validatedUser = await res.json()
+
+  if (res.status !== 200) {
+    // Redireciona para a página de login ou exibe uma mensagem de erro
+    return {
+      redirect: {
+        destination: '/auth',
+        permanent: false
+      }
+    }
+  }
+
+  return {
+    props: {
+      data: validatedUser.user || null
+    }
+  }
+
+  /**
   if (!token) {
     // Redireciona para a página de login ou exibe uma mensagem de erro
     return {
@@ -43,10 +69,10 @@ export async function getServerSideProps(context) {
       }
     }
   }
+  */
 }
 
-export default function Dashboard(props) {
-  const { data } = props
+export default function Dashboard({ data }) {
   const router = useRouter()
 
   const handleLogout = (e) => {
@@ -65,7 +91,9 @@ export default function Dashboard(props) {
         <div className="flex justify-center items-center">
           <div className="flex flex-2">
             <h1 className="flex justify-end">
-              Hello, <span className="font-bold mr-0.5">{'test'}</span>, welcome!
+              <span className="mr-2">Hello,</span>
+              <span className="font-bold"> {data.name} </span>
+              <span className="">, welcome!</span>
             </h1>
           </div>
           <div className="flex-1">
@@ -73,9 +101,9 @@ export default function Dashboard(props) {
           </div>
         </div>
       </div>
-      <div className="flex h-30 bg-gray-200">
+      <div className="flex flex-col h-30 bg-gray-200 p-2">
+        <h1 className="mb-2">Dados do usuário autenticado:</h1>
         <pre>{JSON.stringify(data, null, 2)}</pre>
-        <h1>TEST</h1>
       </div>
     </div>
   )
