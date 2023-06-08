32267bd6aa9313dc0c989465b5635cc89df396f3
angelorubin <angelorubin@gmail.com>
2023-06-08 12:29:00 -0300

    Removendo verificação desnecessário

--- pages/api/auth/index.js
+++ pages/api/auth/index.js
@@ -6,9 +6,7 @@ export default async function handler(req, res) {
   if (req.method === 'POST') {
     try {
       const token = await userAuthentication(email, password)
-      if (token) {
-        res.status(200).json({ token })
-      }
+      res.status(200).json({ token })
     } catch (error) {
       res.status(401).json({ message: 'Acesso não autorizado, credenciais inválidas.' })
     }
