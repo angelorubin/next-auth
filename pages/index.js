00ab96be41cbfe71a30999c56621dea690f17000
Angelo Rubin <angelorubin@gmail.com>
2023-06-12 17:20:47 -0300

    Renomeando index

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,14 @@
 import { useEffect } from 'react'
 import { useRouter } from 'next/router'
 
-const HomePage = () => {
+const Main = () => {
   const router = useRouter()
 
   useEffect(() => {
-    router.push('/auth') // Substitua '/outra-rota' pela rota para a qual deseja redirecionar
+    router.push('/auth')
   }, [])
 
-  return null // Pode retornar null ou qualquer conteúdo que desejar para exibir brevemente na página antes do redirecionamento
+  return null
 }
 
-export default HomePage
+export default Main
