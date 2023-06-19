8e76201c996a011edc8e45f6d9836d3a032a957c
Angelo Rubin <angelorubin@gmail.com>
2023-06-19 14:56:33 -0300

    Implementando imagem do usuário na dashboard

--- pages/dashboard/index.jsx
+++ pages/dashboard/index.jsx
@@ -1,9 +1,10 @@
 import { getServerSession } from 'next-auth/next'
-// import { authOptions } from './api/auth/[...nextauth]'
 import { useSession } from 'next-auth/react'
 import { useRouter } from 'next/router'
 import { signOut } from 'next-auth/react'
 import { AiOutlineLogout } from 'react-icons/ai'
+import { FaUserCircle } from 'react-icons/fa'
+import Icon from '../components/icon'
 
 /**
 export async function validateToken(token) {
@@ -19,14 +20,20 @@ export async function validateToken(token) {
 
 export async function getServerSideProps(context) {
   try {
-    const result = await getServerSession(context.req, context.res)
+    const session = await getServerSession(context.req, context.res)
 
-    const { image, ...userData } = result.user
-
-    return {
-      props: {
-        user: userData
+    if (!session) {
+      return {
+        redirect: {
+          destination: '/auth',
+          permanent: false
+        }
       }
+    }
+
+    // O usuário está logado, você pode acessar as informações do usuário através de session.user
+    return {
+      props: {}
     }
   } catch (error) {
     return {
@@ -59,41 +66,52 @@ export async function getServerSideProps(context) {
     */
 }
 
-export default function Dashboard({ user }) {
-  const { data: session } = useSession()
+export default function Dashboard() {
   const router = useRouter()
+  const session = useSession()
+
+  const image = session?.data?.user?.image ?? false
+  const name = session?.data?.user?.name ?? false
 
   const handleLogout = async (e) => {
     await signOut({ redirect: true, callbackUrl: '/auth' })
   }
 
-  if (session) {
-    return (
-      <div className="flex flex-col w-full h-screen">
-        <div className="flex h-20 w-full bg-gray-300">
-          <div className="flex flex-1 items-center">
-            <span className="text-2xl font-bold m-2">Next</span>
-            <span className="text-2xl">Auth</span>
+  return (
+    <div className="flex flex-col w-full h-screen">
+      <div className="flex h-20 w-full bg-gray-300">
+        <div className="flex flex-1 items-center">
+          <span className="text-2xl font-bold m-2">Next</span>
+          <span className="text-2xl">Auth</span>
+        </div>
+
+        <div className="flex justify-center items-center gap-4">
+          <div className="m-1">
+            {image ? (
+              <img className="border rounded-full h-10 w-10" src={image} alt={'user-image'} />
+            ) : (
+              <Icon size={'2rem'}>
+                <FaUserCircle />
+              </Icon>
+            )}
           </div>
 
-          <div className="flex justify-center items-center">
-            <div className="flex flex-2">
-              <h1 className="flex justify-end">
-                <span className="mr-2">Hello,</span>
-                <span className="font-bold"> {user.name} </span>
-                <span className="">, welcome!</span>
-              </h1>
-            </div>
-            <div className="flex-1">
-              <AiOutlineLogout className="m-5" size={'1.5rem'} onClick={handleLogout} />
-            </div>
+          <div className="flex flex-2">
+            <h1 className="flex justify-end">
+              <span className="font-bold text-xs"> {name && session.data.user.name} </span>
+            </h1>
+          </div>
+
+          <div className="flex-1 m-1">
+            <Icon size={'1.5rem'}>
+              <AiOutlineLogout onClick={handleLogout} />
+            </Icon>
           </div>
         </div>
-        <div className="flex h-full">
-          <pre>{JSON.stringify(user)}</pre>
-        </div>
       </div>
-    )
-  }
-  return <h1>Acesso restrito</h1>
+      <div className="flex h-full">
+        <pre>{JSON.stringify(session, null, 2)}</pre>
+      </div>
+    </div>
+  )
 }
