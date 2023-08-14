c67401c171e0fbe3a256d8d52f0646ca35092e63
angelorubin <angelorubin@gmail.com>
2023-08-14 08:27:36 -0300

    Convertendo rotas para novo modelo do next 13

--- app/api/user/route.js
+++ app/api/user/route.js
@@ -1,6 +1,13 @@
 import { userCreate, usersRetrieve, userRetrieveById } from './service'
 
-export default async function handler(req, res) {
+import { NextResponse } from 'next/server'
+
+export async function GET() {}
+
+export async function POST() {}
+
+/**
+ * export default async function handler(req, res) {
   const { query, params } = req
 
   if (req.method === 'POST') {
@@ -21,3 +28,4 @@ export default async function handler(req, res) {
   }
   res.status(404).json({ error: 'Rota não encontrada' })
 }
+ */
