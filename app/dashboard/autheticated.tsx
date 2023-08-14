74d738e86b52b38d9202ce69d2f51d796fc506a6
Angelo Rubin <angelorubin@gmail.com>
2023-08-14 17:18:00 -0300

    Removendo arquivo não utilizado

--- app/dashboard/autheticated.tsx
+++
@@ -1,6 +0,0 @@
-import { getServerSession } from 'next-auth/next'
-
-export default async function Authenticated() {
-  const session = await getServerSession()
-  return <pre>{JSON.stringify(session, null, 2)}</pre>
-}
