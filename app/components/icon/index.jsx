33ce534318dbbdc062477e8f2593af6edd6bb5e1
Angelo Rubin <angelorubin@gmail.com>
2023-08-18 09:07:00 -0300

    Refatorando arquivo JS para TS

--- app/components/icon/index.jsx
+++
@@ -1,9 +0,0 @@
-import { IconContext } from 'react-icons'
-
-export default function Icon({ size, children }) {
-  return (
-    <IconContext.Provider value={{ size }}>
-      <div>{children}</div>
-    </IconContext.Provider>
-  )
-}
